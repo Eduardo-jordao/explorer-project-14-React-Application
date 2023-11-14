61041b6d6dc4f0d8f8b1cb53434512356d498cd9
eduardo <eduardo@atlaspower.net>
2023-11-14 19:33:45 -0300

    first commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Details () {
+  
+
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Rodrigo Pedro</span>
+
+      <Button title="Login" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
+    </Container>
+  )
+}
